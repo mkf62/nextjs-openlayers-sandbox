8b3e22bad2ec35d4943a2365fe524a65c6656ddc
Molly <mollykfoley@gmail.com>
2026-01-28 11:44:37 -0500

    updates all npm packages

--- nextjs/src/components/Map/layers/icecream.ts
+++ nextjs/src/components/Map/layers/icecream.ts
@@ -1,17 +1,87 @@
 export const icGJson = {
-"type": "FeatureCollection",
-"name": "icecream",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::3857" } },
-"features": [
-{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -11507346.401576763018966, 5647473.123727180063725 ] } },
-{ "type": "Feature", "properties": { "id": 2 }, "geometry": { "type": "Point", "coordinates": [ -10782185.524268252775073, 5162627.188317419961095 ] } },
-{ "type": "Feature", "properties": { "id": 3 }, "geometry": { "type": "Point", "coordinates": [ -10655703.975900487974286, 5402942.130216170102358 ] } },
-{ "type": "Feature", "properties": { "id": 4 }, "geometry": { "type": "Point", "coordinates": [ -11085741.240350883454084, 5166843.239929678849876 ] } },
-{ "type": "Feature", "properties": { "id": 5 }, "geometry": { "type": "Point", "coordinates": [ -10917099.175860533490777, 5293324.78829744271934 ] } },
-{ "type": "Feature", "properties": { "id": 6 }, "geometry": { "type": "Point", "coordinates": [ -10609327.408165641129017, 5702281.794686543755233 ] } },
-{ "type": "Feature", "properties": { "id": 7 }, "geometry": { "type": "Point", "coordinates": [ -10250963.021123645827174, 5246948.220562595874071 ] } },
-{ "type": "Feature", "properties": { "id": 8 }, "geometry": { "type": "Point", "coordinates": [ -10082320.956633294001222, 5478831.059236828237772 ] } },
-{ "type": "Feature", "properties": { "id": 9 }, "geometry": { "type": "Point", "coordinates": [ -10457549.550124324858189, 5428238.439889723435044 ] } },
-{ "type": "Feature", "properties": { "id": 10 }, "geometry": { "type": "Point", "coordinates": [ -10799049.730717286467552, 5580016.297931039705873 ] } }
-]
+    type: 'FeatureCollection',
+    name: 'icecream',
+    crs: { type: 'name', properties: { name: 'urn:ogc:def:crs:EPSG::3857' } },
+    features: [
+        {
+            type: 'Feature',
+            properties: { id: 1 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-11507346.401576763018966, 5647473.123727180063725],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 2 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10782185.524268252775073, 5162627.188317419961095],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 3 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10655703.975900487974286, 5402942.130216170102358],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 4 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-11085741.240350883454084, 5166843.239929678849876],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 5 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10917099.175860533490777, 5293324.78829744271934],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 6 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10609327.408165641129017, 5702281.794686543755233],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 7 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10250963.021123645827174, 5246948.220562595874071],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 8 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10082320.956633294001222, 5478831.059236828237772],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 9 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10457549.550124324858189, 5428238.439889723435044],
+            },
+        },
+        {
+            type: 'Feature',
+            properties: { id: 10 },
+            geometry: {
+                type: 'Point',
+                coordinates: [-10799049.730717286467552, 5580016.297931039705873],
+            },
+        },
+    ],
 }
